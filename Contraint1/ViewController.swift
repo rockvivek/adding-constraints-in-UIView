b56f5830dd4c7b89f5939c786b5a76688ab5151b
vivek shrivastwa <61682978+rockvivek@users.noreply.github.com>
2021-02-05 18:38:23 +0530

    Adding Constraints
    new file

---
+++ Contraint1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contraint1
+//
+//  Created by IPHTECH 20 on 04/02/21.
+//  Copyright © 2021 iPHTech20. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
